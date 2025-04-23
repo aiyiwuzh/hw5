a888726deddb821a1c3df11a6ff5570b6d78490e
Aiyi Wu Zheng <aiyiwuzh@usc.edu>
2025-04-23 03:51:56 +0000

    save local work before merge

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,10 +21,14 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+bool helper(const AvailabilityMatrix& avail, 
+               const size_t dailyNeed,
+               const size_t maxShifts,
+               DailySchedule& sched,
+               std::vector<size_t>& numShifts,
+               size_t day, size_t slot);
 
 // Add your implementation of schedule() and other helper functions here
-
 bool schedule(
     const AvailabilityMatrix& avail,
     const size_t dailyNeed,
@@ -32,14 +36,80 @@ bool schedule(
     DailySchedule& sched
 )
 {
-    if(avail.size() == 0U){
+    if(avail.size() == 0){
         return false;
     }
     sched.clear();
+
     // Add your code below
 
+    size_t numDays = avail.size(); 
+    size_t numWorkers = avail[0].size();
+
+    // initialize schedule with n days with d invalid (empty) slots each
+    for (size_t i = 0; i < numDays; ++i)
+    {
+        std::vector<Worker_T> daySched(dailyNeed, INVALID_ID);
+        sched.push_back(daySched);
+    }
+
+    // initialize vec to keep count of how many shifts each worker has so far 
+    std::vector<size_t> numShifts(numWorkers, 0);
+
+    // recursive call 
+    return helper(avail, dailyNeed, maxShifts, sched, numShifts, 0, 0);
+
+}
 
 
+// helper function implementation
+bool helper(const AvailabilityMatrix& avail, 
+               const size_t dailyNeed,
+               const size_t maxShifts,
+               DailySchedule& sched,
+               std::vector<size_t>& numShifts,
+               size_t day, size_t slot)
+{
+    size_t numDays = avail.size(); 
+    size_t numWorkers = avail[0].size(); 
 
+    // base case: all days are scheduled
+    if (day == numDays)
+    {
+        return true; 
+    }
+
+    // day is filled, move onto the next one
+    if (slot == dailyNeed)
+    {
+        return helper(avail, dailyNeed, maxShifts, sched, numShifts, day + 1, 0);
+    }
+
+    // try each worker for slot on current day 
+    for (Worker_T worker = 0; worker < numWorkers; ++worker)
+    {
+        // make sure worker is available on day, have not exceeded shift limit
+        // also make sure worker is not already scheduled for current day
+        if (avail[day][worker] && numShifts[worker] < maxShifts
+            && std::find(sched[day].begin(), sched[day].end(), worker) == sched[day].end())
+        {
+            // assign worker to slot 
+            sched[day][slot] = worker; 
+            numShifts[worker]++; 
+
+            // recursive call to check the next slot 
+            if (helper(avail, dailyNeed, maxShifts, sched, numShifts, day, slot + 1))
+            {
+                return true; 
+            }
+
+            // backtrack if assignment did not work out 
+            numShifts[worker]--;
+            sched[day][slot] = INVALID_ID;
+        }
+    }
+
+    // cannot find a way to fit worker into this slot 
+    return false; 
 }
 
